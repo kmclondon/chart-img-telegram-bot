6301c8b1f508baf7a844929eb945e984939002c9
Onder OZCAN <betterwithbetting@gmail.com>
2024-05-14 03:58:36 +0300

    Tradingview sessionid removed

--- src/helper/chartimg.js
+++ src/helper/chartimg.js
@@ -83,13 +83,7 @@ function postPayloadRESTv2(path, apiKey, payload, opt = {}) {
 
   const headers = {
     'x-api-key': apiKey,
-    'tradingview-session-id': '7sztuwjo4vjv7a88gg8rhvd3hbb1dyao',
-    'tradingview-session-id-sign' : 'MkSf/A3eC6c6OnJ7IDDmvnbji4+Ak7QI3q1vXeYCAvw=',
-    'content-type': 'application/json',
-  }
-
-  if (opt.sessionId) {
-    headers['tradingview-session-id'] = "7sztuwjo4vjv7a88gg8rhvd3hbb1dyao"
+    'content-type': 'application/json'
   }
 
   return fetch(`${BASE_API_URL}/v2/${path}`, {
